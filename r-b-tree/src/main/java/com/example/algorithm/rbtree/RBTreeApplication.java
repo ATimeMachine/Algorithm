6ed3777720cbe4639fdeb328cb84c269879d50f7
chenjian <chenjian@suneee.com>
2018-08-31 11:38:01 +0800

    红黑数

---
+++ r-b-tree/src/main/java/com/example/algorithm/rbtree/RBTreeApplication.java
@@ -0,0 +1,12 @@
+package com.example.algorithm.rbtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RBTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RBTreeApplication.class, args);
+	}
+}
