0ed9d8f3251e70b4570bbcdbdf32744bf3725fdf
chenjian <chenjian@suneee.com>
2018-09-04 16:09:37 +0800

    红黑数完成

--- r-b-tree/src/main/java/com/example/algorithm/rbtree/implement/TestTree.java
+++ r-b-tree/src/main/java/com/example/algorithm/rbtree/implement/TestTree.java
@@ -1,5 +1,9 @@
 package com.example.algorithm.rbtree.implement;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
 /**
  * FileName: TestTree
  * Author:   SunEee
@@ -11,27 +15,46 @@ public class TestTree {
         int length = 100000;
         RedBlackTree rb = new RedBlackTree();
         String[] arr = new String[length];
+        List<String> strings = new ArrayList<>();
         String tmp;
         for (int i = 0; i < length; i++) {
             tmp = String.valueOf(i);
             rb.put(i, tmp);
             arr[i] = tmp;
+            strings.add(tmp);
         }
 
-        String key = "99999";
+        String key = "65535";
+
+        getTime("数组方式:",() ->arr(arr,key) );
+        getTime("集合:",()-> forLoop(strings,key));
+        getTime("红黑树:",()-> rb.get(Integer.valueOf(key)));
+
+    }
+
+    private static <T>void getTime(String name, Supplier<T> supplier){
         long start = System.nanoTime();
+        System.out.println(supplier.get());
+        long end = System.nanoTime() - start;
+        System.out.println(name + end +"--长度--" + String.valueOf(end).length() );
+    }
+
+    private static String arr(String[] arr, String key) {
         for (String s : arr) {
             if (s.equals(key)) {
-                System.out.println(s);
-                break;
+                return s;
             }
         }
-        long end = System.nanoTime();
-        System.out.println("数组方式:" + (end - start));
-
-        start = System.nanoTime();
-        System.out.println(rb.get(Integer.valueOf(key)));
-        end = System.nanoTime();
-        System.out.println("红黑树:" + (end - start));
+        return null;
     }
+
+    private static String forLoop(List<String> strings, String key) {
+        for (String string : strings) {
+            if (string.equals(key)) {
+                return string;
+            }
+        }
+        return null;
+    }
+
 }
